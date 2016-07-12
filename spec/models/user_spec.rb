627aae5b3314a21927b5740bbf5db339a9406182
Juan Carlos Mercado <barkatu.83@gmail.com>
2016-07-12 17:14:41 -0500

    basic user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.build(:user) }
+  subject { user }
+  context 'validations' do
+    it { should be_valid }
+  end
 end
