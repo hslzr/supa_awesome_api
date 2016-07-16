892f8f8f67d2a0848efb24d5c1a1e47a61262a8a
Juan Carlos Mercado <barkatu.83@gmail.com>
2016-07-16 14:33:00 -0500

    Api consumer started

--- config/application.rb
+++ config/application.rb
@@ -33,10 +33,14 @@ module SupaAwesomeApi
       g.helper false
     end
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :options]
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
       end
     end
 
